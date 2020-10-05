319479e5195d46fee8c1d2cba90d68efe2088d72
Keerati <algobaleno.un@gmail.com>
2020-10-05 18:35:25 +0700

    Add prettier file config & update content

--- src/App.js
+++ src/App.js
@@ -3,61 +3,61 @@ import { Terminal } from './component';
 import './App.css';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            command: ''
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      command: '',
+    };
+  }
 
-    render() {
-        return (
-            <div>
-                <div className="container">
-                    <div className="title">Keerati's Profile</div>
-                    <div>
-                        <Terminal command={this.state.command} />
-                    </div>
-                    <div className="tags-section">
-                        <div className="tags-title">>_ COMMAND LIST</div>
-                        <ul className="tags">
-                            <li>
-                                <a href="#intro">Introduce Myself (intro)</a>
-                            </li>
-                            <li>
-                                <a href="#edu">Education (edu)</a>
-                            </li>
-                            <li>
-                                <a href="#lang">Language (lang)</a>
-                            </li>
-                            <li>
-                                <a href="#stskill">Strong Skill (stskill)</a>
-                            </li>
-                            <li>
-                                <a href="#pskill">Programming Skill (pskill)</a>
-                            </li>
-                            <li>
-                                <a href="#sskill">Soft Skill (sskill)</a>
-                            </li>
-                            {/* <li><a>Awards (awards)</a></li> */}
-                            <li>
-                                <a href="#exp">Experience (exp)</a>
-                            </li>
-                            <li>
-                                <a href="#contact">Contact (contact)</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="footer-section">
-                    © 2018 Keerati | <a href="https://github.com/handtevada/portfolio">Github</a> |{' '}
-                    <a href="https://medium.com/@directorys/%E0%B8%AA%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%87-portfolio-web-app-%E0%B9%81%E0%B8%9A%E0%B8%9A%E0%B9%80%E0%B8%81%E0%B8%A3%E0%B9%8B%E0%B9%86-%E0%B8%AA%E0%B9%84%E0%B8%95%E0%B8%A5%E0%B9%8C%E0%B9%82%E0%B8%9B%E0%B8%A3%E0%B9%81%E0%B8%81%E0%B8%A3%E0%B8%A1%E0%B9%80%E0%B8%A1%E0%B8%AD%E0%B8%A3%E0%B9%8C-8c82175a71bb">
-                        Blog
-                    </a>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div className='container'>
+          <div className='title'>Keerati's Profile</div>
+          <div>
+            <Terminal command={this.state.command} />
+          </div>
+          <div className='tags-section'>
+            <div className='tags-title'>{'>'}_ COMMAND LIST</div>
+            <ul className='tags'>
+              <li>
+                <a href='#intro'>Introduce Myself (intro)</a>
+              </li>
+              <li>
+                <a href='#edu'>Education (edu)</a>
+              </li>
+              <li>
+                <a href='#lang'>Language (lang)</a>
+              </li>
+              <li>
+                <a href='#stskill'>Strong Skill (stskill)</a>
+              </li>
+              <li>
+                <a href='#pskill'>Programming Skill (pskill)</a>
+              </li>
+              <li>
+                <a href='#sskill'>Soft Skill (sskill)</a>
+              </li>
+              {/* <li><a>Awards (awards)</a></li> */}
+              <li>
+                <a href='#exp'>Experience (exp)</a>
+              </li>
+              <li>
+                <a href='#contact'>Contact (contact)</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='footer-section'>
+          © 2018 Keerati | <a href='https://github.com/handtevada/portfolio'>Github</a> |{' '}
+          <a href='https://medium.com/@directorys/%E0%B8%AA%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%87-portfolio-web-app-%E0%B9%81%E0%B8%9A%E0%B8%9A%E0%B9%80%E0%B8%81%E0%B8%A3%E0%B9%8B%E0%B9%86-%E0%B8%AA%E0%B9%84%E0%B8%95%E0%B8%A5%E0%B9%8C%E0%B9%82%E0%B8%9B%E0%B8%A3%E0%B9%81%E0%B8%81%E0%B8%A3%E0%B8%A1%E0%B9%80%E0%B8%A1%E0%B8%AD%E0%B8%A3%E0%B9%8C-8c82175a71bb'>
+            Blog
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/component/terminal/Terminal.js
+++ src/component/terminal/Terminal.js
@@ -2,179 +2,211 @@ import React, { Component } from 'react';
 import Terminal from 'react-bash';
 
 export const intro = {
-    exec: ({ structure, history, cwd }, command) => {
-        return {
-            structure,
-            cwd,
-            history: history
-                .concat({ value: ">_ Hi, I'm Keerati Yuonghirun, Tung" })
-                .concat({ value: '>_ November 21, 1990, Bangkok, THAILAND' })
-                .concat({ value: '>_ Grew up in Kamphaeng Phet like normal guy.' })
-                .concat({ value: '>_ Now, .Net C# and Node JS Developer, Full Stack, Blogger.' })
-                .concat({ value: '>_ Future, I wanna be YouTuber' })
-                .concat({ value: '>' })
-        };
-    }
+  exec: ({ structure, history, cwd }, command) => {
+    return {
+      structure,
+      cwd,
+      history: history
+        .concat({ value: ">_ Hi, I'm Keerati Yuonghirun, Tung" })
+        .concat({ value: '>_ November 21, 1990, Bangkok, THAILAND' })
+        .concat({ value: '>_ Grew up in Kamphaeng Phet like normal guy.' })
+        .concat({
+          value: '>_ Now, .Net C# and Node JS Developer, Full Stack, Blogger.',
+        })
+        .concat({ value: '>_ Future, I wanna be YouTuber' })
+        .concat({ value: '>' }),
+    };
+  },
 };
 
 export const edu = {
-    exec: ({ structure, history, cwd }, command) => {
-        return {
-            structure,
-            cwd,
-            history: history
-                .concat({ value: '>_ Bachelor of Engineering Program' })
-                .concat({ value: '>_ Computer Engineering, Naresuan University (Phitsanulok)' })
-                .concat({ value: '>_ Grade point average : 2.7' })
-                .concat({ value: '>_' })
-                .concat({ value: '>_ High School' })
-                .concat({ value: '>_ Prankratai Pittayakom School (Kamphaeng Phet)' })
-                .concat({ value: '>' })
-        };
-    }
+  exec: ({ structure, history, cwd }, command) => {
+    return {
+      structure,
+      cwd,
+      history: history
+        .concat({ value: '>_ Bachelor of Engineering Program' })
+        .concat({
+          value: '>_ Computer Engineering, Naresuan University (Phitsanulok)',
+        })
+        .concat({ value: '>_ Grade point average : 2.7' })
+        .concat({ value: '>_' })
+        .concat({ value: '>_ High School' })
+        .concat({ value: '>_ Prankratai Pittayakom School (Kamphaeng Phet)' })
+        .concat({ value: '>' }),
+    };
+  },
 };
 
 export const lang = {
-    exec: ({ structure, history, cwd }, command) => {
-        return {
-            structure,
-            cwd,
-            history: history
-                .concat({ value: ">_ THAI : 4/5 => WHY! I'm a Programmer" })
-                .concat({ value: '>_ ENGLISH : 3/5 => Can survive in foreign country. Read and write are pretty good.' })
-                .concat({ value: '>_ JAPANESE : 2/5 => Simple sentence, Introduce myself' })
-                .concat({ value: '>' })
-        };
-    }
+  exec: ({ structure, history, cwd }, command) => {
+    return {
+      structure,
+      cwd,
+      history: history
+        .concat({ value: ">_ THAI : 4/5 => WHY! I'm a Programmer" })
+        .concat({
+          value:
+            '>_ ENGLISH : 3/5 => Can survive in foreign country. Read and write are pretty good.',
+        })
+        .concat({
+          value: '>_ JAPANESE : 2/5 => Simple sentence, Introduce myself',
+        })
+        .concat({ value: '>' }),
+    };
+  },
 };
 
 export const stskill = {
-    exec: ({ structure, history, cwd }, command) => {
-        return {
-            structure,
-            cwd,
-            history: history
-                .concat({ value: '>_ Learn fast Eat faster' })
-                .concat({ value: ">_ Can talk just one word for a day 'KRUB' :)" })
-                .concat({ value: ">_ Succeed in quitting Facebook" })
-                .concat({ value: '>' })
-        };
-    }
+  exec: ({ structure, history, cwd }, command) => {
+    return {
+      structure,
+      cwd,
+      history: history
+        .concat({ value: '>_ Learn fast Eat faster' })
+        .concat({ value: ">_ Can talk just one word for a day 'KRUB' :)" })
+        .concat({ value: '>_ Succeed in quitting Facebook' })
+        .concat({ value: '>' }),
+    };
+  },
 };
 
 export const pskill = {
-    exec: ({ structure, history, cwd }, command) => {
-        return {
-            structure,
-            cwd,
-            history: history
-                .concat({
-                    value: '>_ Now, Expertise about .Net C#, Node JS, Full Stack Developer'
-                })
-                .concat({ value: '>_ ' })
-                .concat({ value: '>_ Backend  => 9/10' })
-                .concat({ value: '>_ Frontend => 8/10' })
-                .concat({ value: '>_ Windows Server => 9/10' })
-                .concat({ value: '>_ Linux Server => 8/10' })
-                .concat({ value: '>_ ' })
-                .concat({ value: '>_ PROGRAMMING LANGUAGE' })
-                .concat({ value: '>_ Advanced => C#, Node JS' })
-                .concat({ value: '>_ Intermediate => Java, C++, PHP' })
-                .concat({ value: '>_ Beginner => Python' })
-                .concat({ value: '>_ ' })
-                .concat({ value: '>_ WEB STACK & TOOLS' })
-                .concat({
-                    value: '>_ React, Node.js, jQuery, Lumen, WordPress, MySql, MongoDB, MicrosoftSQL, Firebase, Git, Docker, Kafka, Kong, GCP, GKE'
-                })
-                .concat({ value: '>_ ' })
-                .concat({ value: '>_ INTERESTED NOW' })
-                .concat({ value: '>_ UX/UI, Artificial Intelligence, Cloud Technology, Politics and Podcasts.' })
-                .concat({ value: '>' })
-        };
-    }
+  exec: ({ structure, history, cwd }, command) => {
+    return {
+      structure,
+      cwd,
+      history: history
+        .concat({
+          value: '>_ Now, Expertise about .Net C#, Node JS, Full Stack Developer',
+        })
+        .concat({ value: '>_ ' })
+        .concat({ value: '>_ Backend  => 9/10' })
+        .concat({ value: '>_ Frontend => 9/10' })
+        .concat({ value: '>_ Windows Server => 9/10' })
+        .concat({ value: '>_ Linux Server => 8/10' })
+        .concat({ value: '>_ ' })
+        .concat({ value: '>_ PROGRAMMING LANGUAGE' })
+        .concat({ value: '>_ Advanced => C#, Node JS' })
+        .concat({ value: '>_ Intermediate => Java, C++, PHP' })
+        .concat({ value: '>_ Beginner => Python' })
+        .concat({ value: '>_ ' })
+        .concat({ value: '>_ WEB STACK & TOOLS' })
+        .concat({
+          value:
+            '>_ React, Node.js, jQuery, Lumen, WordPress, MySql, MongoDB, MicrosoftSQL, Firebase, Git, Docker, Kafka, Kong, GCP, GKE',
+        })
+        .concat({ value: '>_ ' })
+        .concat({ value: '>_ INTERESTED NOW' })
+        .concat({
+          value: '>_ UX/UI, Artificial Intelligence, Cloud Technology, Politics and Podcasts.',
+        })
+        .concat({ value: '>' }),
+    };
+  },
 };
 
 export const sskill = {
-    exec: ({ structure, history, cwd }, command) => {
-        return {
-            structure,
-            cwd,
-            history: history
-                .concat({ value: '>_ Photographer => IG : Directorysc (everyday story)' })
-                .concat({ value: '>_ Medium => medium.com/@directorys (About technology or programming)' })
-                .concat({ value: '>_ Blogger => engineerindy.com (About review and general thing)' })
-                .concat({ value: '>_ Sports => Tennis, Table Tennis, Go' })
-                .concat({ value: '>_ Games => ROV (For now)' })
-                .concat({ value: '>_ Treader => Stock, Crypto' })
-                .concat({ value: '>' })
-        };
-    }
+  exec: ({ structure, history, cwd }, command) => {
+    return {
+      structure,
+      cwd,
+      history: history
+        .concat({
+          value: '>_ Photographer => IG : Directorysc (everyday story)',
+        })
+        .concat({
+          value: '>_ Medium => medium.com/@directorys (About technology or programming)',
+        })
+        .concat({
+          value: '>_ Blogger => engineerindy.com (About review and general thing)',
+        })
+        .concat({ value: '>_ Sports => Tennis, Table Tennis, Go' })
+        .concat({ value: '>_ Games => ROV (For now)' })
+        .concat({ value: '>_ Treader => Stock, Crypto' })
+        .concat({ value: '>' }),
+    };
+  },
 };
 
 export const awards = {
-    exec: ({ structure, history, cwd }, command) => {
-        return {
-            structure,
-            cwd,
-            history: history.concat({ value: '>_ ???' }).concat({ value: '>' })
-        };
-    }
+  exec: ({ structure, history, cwd }, command) => {
+    return {
+      structure,
+      cwd,
+      history: history.concat({ value: '>_ ???' }).concat({ value: '>' }),
+    };
+  },
 };
 
 export const exp = {
-    exec: ({ structure, history, cwd }, command) => {
-        return {
-            structure,
-            cwd,
-            history: history
-                .concat({ value: '>_ 2019 - Now : Software Engineer, Tangerine Co.,Ltd.' })
-                .concat({ value: '>_ 2015 - 2019 : C#.NET Developer, Piesoft Co.,Ltd.' })
-                .concat({ value: '>_ 2013 - 2015 : Java Developer, AR Soft - Advanced Research Group Co.,Ltd.' })
-                .concat({ value: '>_ 2012 : Apprentice : TESA Hyper Camp' })
-                .concat({ value: '>' })
-        };
-    }
+  exec: ({ structure, history, cwd }, command) => {
+    return {
+      structure,
+      cwd,
+      history: history
+        .concat({ value: '>_ 2020 - Now : Outsource' })
+        .concat({
+          value: '>_ 2019 - 2020 : Software Engineer, Tangerine Co.,Ltd.',
+        })
+        .concat({
+          value: '>_ 2015 - 2019 : C#.NET Developer, Piesoft Co.,Ltd.',
+        })
+        .concat({
+          value: '>_ 2013 - 2015 : Java Developer, AR Soft - Advanced Research Group Co.,Ltd.',
+        })
+        .concat({ value: '>_ 2012 : Apprentice : TESA Hyper Camp' })
+        .concat({ value: '>' }),
+    };
+  },
 };
 
 export const contact = {
-    exec: ({ structure, history, cwd }, command) => {
-        return {
-            structure,
-            cwd,
-            history: history
-                .concat({ value: '>_ Email : algobaleno.un@gmail.com' })
-                .concat({ value: '>_ Twitter : @Algobaleno' })
-                .concat({ value: '>_ Instagram : Directorys' })
-                .concat({ value: '>_ Medium : medium.com/@directorys' })
-                .concat({ value: '>_ Github : github.com/handtevada' })
-                .concat({ value: '>' })
-        };
-    }
+  exec: ({ structure, history, cwd }, command) => {
+    return {
+      structure,
+      cwd,
+      history: history
+        .concat({ value: '>_ Email : algobaleno.un@gmail.com' })
+        .concat({ value: '>_ Twitter : @Algobaleno' })
+        .concat({ value: '>_ Instagram : Directorys' })
+        .concat({ value: '>_ Medium : medium.com/@directorys' })
+        .concat({ value: '>_ Github : github.com/handtevada' })
+        .concat({ value: '>' }),
+    };
+  },
 };
 
 class terminal extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    render() {
-        const extensions = {
-            intro,
-            edu,
-            lang,
-            stskill,
-            pskill,
-            sskill,
-            awards,
-            exp,
-            contact
-        };
-        const history = [{ value: 'Welcome to my profile!' }].concat({
-            value: 'Please type the command'
-        });
-        return <Terminal prefix={'portfolio'} theme={Terminal.Themes.DARK} history={history} extensions={extensions} />;
-    }
+  render() {
+    const extensions = {
+      intro,
+      edu,
+      lang,
+      stskill,
+      pskill,
+      sskill,
+      awards,
+      exp,
+      contact,
+    };
+    const history = [{ value: 'Welcome to my profile!' }].concat({
+      value: 'Please type the command',
+    });
+    return (
+      <Terminal
+        prefix={'portfolio'}
+        theme={Terminal.Themes.DARK}
+        history={history}
+        extensions={extensions}
+      />
+    );
+  }
 }
 
 export default terminal;
