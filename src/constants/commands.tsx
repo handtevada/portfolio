37036e91b69ebd4bf982890c61ae8adcc36799c7
keerati <algobaleno.un@gmail.com>
2025-12-29 02:34:46 +0700

    fix(commands): improve formatting and consistency in command descriptions

--- src/constants/commands.tsx
+++ src/constants/commands.tsx
@@ -20,8 +20,15 @@ export const commands: Command[] = [
       <span>
         <br />
         Hello! I'm Keerati Yuonghirun (Tung), <br />
-        a passionate software developer with a love for creating innovative solutions. <br />
-        21 November 1990 ({cDate} old). <br />
+        <br />
+        - A passionate software developer with a love for creating innovative solutions. <br />
+        <br />- 21 November 1990 ({cDate} old). <br />
+        <br />
+        - Full Stack Developer with expertise in .Net C#, Node JS, and various web technologies.{' '}
+        <br />
+        <br />
+        - 10+ years of experience in software development, working on diverse projects and teams.
+        <br />
         <br />
         Welcome to my portfolio terminal!
         <br />
@@ -77,24 +84,29 @@ export const commands: Command[] = [
         Now, Expertise about .Net C#, Node JS, Full Stack Developer
         <br />
         <br />
-        - Full Stack : Building systems from the ground up, maintaining and evolving existing
+        - Full Stack: Building systems from the ground up, maintaining and evolving existing
         solutions, integrating third-party services, researching and adopting new technologies, and
         coaching team members.
         <br />
-        - Windows Server : Understanding system operations and knowing how to diagnose and resolve
+        <br />
+        - Windows Server: Understanding system operations and knowing how to diagnose and resolve
         issues. <br />
-        - Linux Server : Familiar with basic Linux commands and experienced in researching
+        <br />- Linux Server: Familiar with basic Linux commands and experienced in researching
         solutions. <br />
-        - Database : Experienced in designing database schemas, writing complex queries, and
+        <br />
+        - Database: Experienced in designing database schemas, writing complex queries, and
         optimizing database performance. Familiar with MySql, MongoDB, MicrosoftSQL and Redis.
         <br />
-        - Cloud : Experienced in deploying and managing applications on cloud platforms like GCP and
+        <br />
+        - Cloud: Experienced in deploying and managing applications on cloud platforms like GCP and
         Azure.
         <br />
-        - DevOps : Familiar with CI/CD pipelines, containerization using Docker and infrastructure
-        as code (IaC) tools.
         <br />
-        - Web stack & Tools : C#, React, Node.js, Lumen, WordPress, Firebase, Git, Docker, K8s,
+        - DevOps: Familiar with CI/CD pipelines, containerization using Docker and infrastructure as
+        code (IaC) tools.
+        <br />
+        <br />
+        - Web stack & Tools: C#, React, Node.js, Lumen, WordPress, Firebase, Git, Docker, K8s,
         Kafka, Kong, Jenkins and more.
         <br />
       </span>
@@ -107,13 +119,13 @@ export const commands: Command[] = [
     result: (
       <span>
         <br />
-        - Photographer : IG (algobalen0)
+        - Photographer: IG (algobalen0)
         <br />
-        - Blogger : medium.com/@directorys (About technology or programming)
+        - Blogger: medium.com/@directorys (About technology or programming)
         <br />
-        - Sport : Football, Running, Table Tennis <br />
-        - Game : Go, ROV, Roblox <br />
-        - Treader : Stock, Crypto, Gold <br />
+        - Sport: Football, Running, Table Tennis <br />
+        - Game: Go, ROV, Roblox <br />
+        - Trader: Stock, Crypto, Gold <br />
       </span>
     ),
   },
@@ -124,12 +136,18 @@ export const commands: Command[] = [
     result: (
       <span>
         <br />
-        - 2024 - Present: Team Lead Developer at Clicknext Co.,Ltd. <br />
-        - 2023 - 2024: Senior Software Developer at Zortout Co.,Ltd <br />
-        - 2020 - 2023: Senior Software Developer at True Co.,Ltd. <br />
-        - 2019 - 2020: Software Engineer, Tangerine Co.,Ltd. <br />
-        - 2015 - 2019: C#.NET Developer, Piesoft Co.,Ltd. <br />
-        - 2013 - 2015: Java Developer, AR Soft - Advanced Research Group Co.,Ltd. <br />
+        - 2024-Present: Team Lead Developer at Clicknext Co.,Ltd. <br />
+        <br />
+        - 2023-2024: Senior Software Developer at Zortout Co.,Ltd <br />
+        <br />
+        - 2020-2023: Senior Software Developer at True Co.,Ltd. <br />
+        <br />
+        - 2019-2020: Software Engineer, Tangerine Co.,Ltd. <br />
+        <br />
+        - 2015-2019: C#.NET Developer, Piesoft Co.,Ltd. <br />
+        <br />
+        - 2013-2015: Java Developer, AR Soft - Advanced Research Group Co.,Ltd. <br />
+        <br />
         - 2012: Apprentice : TESA Hyper Camp <br />
       </span>
     ),
@@ -141,8 +159,8 @@ export const commands: Command[] = [
     result: (
       <span>
         <br />
-        - Email : algobaleno.un@gmail.com <br />
-        - X : @Algobaleno
+        - Email: algobaleno.un@gmail.com <br />
+        - X: @Algobaleno
         <br />
         - Instagram: Directorys <br />
         - Medium: medium.com/@directorys <br />
